799535874e072435ee2df0e2bdaa12425daf5678
Anycen <x@anycen.com>
2017-04-25 21:54:13 +0800

    First commit.

---
+++ AnyLib/AnyH/anyh.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+extern int flag_send;
+extern int flag_testsend;
+extern uint8_t testsend[21];
+extern int flag_error_getDroneVersion;
+extern int flag_dyhx_switch;
+extern uint16_t mobile_data_t[100];
+
+extern int co;
+extern int so2;
+extern int o3;
+extern int no2;
+extern int pm25;
+extern int pm10;
